dc5c5eabc77d3751eaf8291f11b920328e4f6084
NaiveKim <106145866+NaiveKim@users.noreply.github.com>
2022-08-08 10:05:20 +0900

    Data check

--- 01_climate_API.py
+++ 01_climate_API.py
@@ -3,23 +3,36 @@ import pandas as pd
 import time
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
+from urllib.parse import urlencode, unquote
 
-
-yesterday = datetime.today() - timedelta(1)
-yesterday_date = yesterday.strftime("%Y-%m-%d")
-
-end_date =  str(yesterday_date).replace("-","")
-service_key = "I1MxqWJqYg%2FV1XIhDYJ2RMYQImlp7CnkXyQNpy6PmFGSYeSuRdJuNQdCQZJf9PoK0l6elGZ3pIwPy9RZnpq6dQ%3D%3D"
-
-url = 'http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList'
+# yesterday = datetime.today() - timedelta(1)
+# yesterday_date = yesterday.strftime("%Y-%m-%d")
+#
+# end_date =  str(yesterday_date).replace("-","")
+# service_key = "I1MxqWJqYg/V1XIhDYJ2RMYQImlp7CnkXyQNpy6PmFGSYeSuRdJuNQdCQZJf9PoK0l6elGZ3pIwPy9RZnpq6dQ=="
+#
+# url = 'http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList'
 # params ={'serviceKey' : service_key,
 #          'pageNo' : '1', 'numOfRows' : '10', 'dataType' : 'XML', 'dataCd' : 'ASOS', 'dateCd' : 'HR',
 #          'startDt' : '20160501', 'startHh' : '01', 'endDt' : end_date, 'endHh' : '01', 'stnIds' : '108' }
+#
+# response = requests.get(url)
+# content = response.content
+# dict = xmltodict.parse(content)
+# jsonString = json.dumps(dict)
+#
+# content = requests.get(url).content
+# print(content)
 
-response = requests.get(url)
-content = response.content
-dict = xmltodict.parse(content)
-jsonString = json.dumps(dict)
 
-content = requests.get(url).content
-print(content)
+url = 'http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList'
+params ={'serviceKey' : 'IJkDF/XrzABk9OGODmat0ZCNoGNUFpawi9nRyHwErjSKfSYCdcd7Nje45MY1ESL3wH+USjpwxa3YWXK37l9yow==',
+         'pageNo' : '1', 'numOfRows' : '10', 'dataType' : 'XML', 'dataCd' : 'ASOS', 'dateCd' : 'HR',
+         'startDt' : '20100101', 'startHh' : '01', 'endDt' : '20100601', 'endHh' : '01', 'stnIds' : '108' }
+# params ={'serviceKey' : 'IJkDF%2FXrzABk9OGODmat0ZCNoGNUFpawi9nRyHwErjSKfSYCdcd7Nje45MY1ESL3wH%2BUSjpwxa3YWXK37l9yow%3D%3D',
+#          'pageNo' : '1', 'numOfRows' : '10', 'dataType' : 'XML', 'dataCd' : 'ASOS', 'dateCd' : 'HR',
+#          'startDt' : '20100101', 'startHh' : '01', 'endDt' : '20100601', 'endHh' : '01', 'stnIds' : '108' }
+
+response = requests.get(url, params=params)
+print(response.content)
+print(response.text)
--- 06_RNN_LTRM.py
+++ 06_RNN_LTRM.py
@@ -4,13 +4,29 @@ from keras.layers import Dense, LSTM
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
 import numpy as np
-
+import seaborn as sns
 
 mosquito_df = pd.read_csv('mosquito_active_rate/mosquito_active_rate_20160101_20220719.csv')
 
-X = mosquito_df.drop(columns = ['date', 'mosquito_Indicator'])
+X = mosquito_df.drop(columns = ['date','rain(mm)', 'mosquito_Indicator'])
 Y = mosquito_df['mosquito_Indicator']
+
+rain_data = mosquito_df['rain(mm)']
+
+
+
+
+minmax = MinMaxScaler()
+
+minmax.fit(X)
+
+X_scaled = minmax.transform(X)
+
+X_scaled = pd.DataFrame(X_scaled)
+X = pd.concat([rain_data, X_scaled], axis = 1)
 
 X_values = X.values
 
@@ -21,12 +37,11 @@ datalist.append(X_values)
 X = np.array(datalist)
 #
 #
-print(X)
-# data_reshape = data_array.reshape(1,40)
 
 
 
-
+#
+#
 print('x shape:', X.shape)
 print('y shape:', Y.shape)
 
@@ -36,6 +51,8 @@ X = X.reshape(X.shape[1], 4,1)
 print('x shape:',X.shape)
 print(X)
 
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=False)
+
 model=Sequential()
 model.add(LSTM(30,activation='relu',input_shape=(4,1)))
 
@@ -43,8 +60,14 @@ model.add(Dense(1))
 
 model.summary()
 
-model.compile(optimizer = 'adam', loss = 'mse')
-model.fit(X,Y, epochs=100, batch_size = 1)
+early_stop = EarlyStopping(monitor='val_loss', mode = 'min', verbose=1, patience = 5)
+checkpoint = ModelCheckpoint('the_best.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+
+model.compile(optimizer = 'adam', loss = 'mse', metrics=['acc'])
+model.fit(x_train,y_train,validation_data= (x_test, y_test), epochs=100, batch_size = 1)
+# print(model.evaluate(x_test, y_test))
+
+
 x_input = array([11.5, 27.1, 24.3, 30.7])
 x_input = x_input.reshape((1,4,1))
 
